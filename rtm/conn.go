b2b96bd83e2beadcb7df643390584157b9ce9f2d
Ben Bariteau <firba1@gmail.com>
2015-07-29 09:37:32 -0700

    Added functionality for @user mentions.

--- rtm/conn.go
+++ rtm/conn.go
@@ -1,6 +1,7 @@
 package rtm
 
 import (
+	"regexp"
 	"strings"
 
 	"github.com/firba1/slack"
@@ -68,13 +69,45 @@ func (c *Conn) SendMessage(text, channel string) error {
 	return c.conn.WriteJSON(msg)
 }
 
+var escapeRegex = regexp.MustCompile("<(.*?)>")
+
 /*
 UnescapeMessage takes in the escape string text of a message and returns a new string that appears as it would to a user.
 
 UnescapeMessage does so by parsing escape sequences according to <https://api.slack.com/docs/formatting> and substituting the appropriate user-facing junk (e.g. <@UABC123> would become @firba1, assuming there's a user named firba1 with the user ID UABC123).
 */
 func (c Conn) UnescapeMessage(message string) string {
-	// first replace all html entity escapes
+	message = escapeRegex.ReplaceAllStringFunc(message, func(match string) string {
+		// remove < and > from each end
+		escape := match[1 : len(match)-1]
+		escapeParts := strings.Split(escape, "|")
+
+		// this is a case we don't recognize, just return the original match
+		if len(escapeParts) > 2 {
+			return match
+		}
+
+		if len(escapeParts) == 2 {
+			return escapeParts[1]
+		}
+
+		// since there's no alias, now it's time for idenitifier lookup
+		escape = escapeParts[0]
+
+		switch {
+		case escape[0:2] == "@U":
+			// user link
+			user, ok := c.Users[escape[1:]]
+			if ok {
+				// mentions always have an "@" preceding the username
+				return "@" + user.Name
+			}
+		}
+		// if we were unable to unescape this, just return the original match
+		return match
+	})
+
+	// finally replace all html entity escapes
 	message = strings.Replace(message, "&amp;", "&", -1)
 	message = strings.Replace(message, "&lt;", "<", -1)
 	message = strings.Replace(message, "&gt;", ">", -1)
--- rtm/conn_test.go
+++ rtm/conn_test.go
@@ -3,6 +3,8 @@ package rtm
 import (
 	"testing"
 
+	"github.com/firba1/slack"
+
 	"github.com/firba1/util/assert"
 )
 
@@ -14,9 +16,17 @@ func TestUnescapeMessage(t *testing.T) {
 		{"Simon &amp; Garfunkel", "Simon & Garfunkel"},
 		{"Independence Day &gt; Stargate", "Independence Day > Stargate"},
 		{"):&lt;", "):<"},
+		{"<@U123A56BC>++", "@fart++"},
 	}
 
-	conn := Conn{}
+	conn := Conn{
+		Users: map[string]slack.User{
+			"U123A56BC": slack.User{
+				ID:   "U123A56BC",
+				Name: "fart",
+			},
+		},
+	}
 
 	for _, test := range tests {
 		assert.Equal(t, conn.UnescapeMessage(test.in), test.out)
